22c2bdaf23f2305f5ab99de71021fdcf12164bc9
nj.ara1995 <nj.ara1995@gmail.com>
2023-03-23 11:32:41 -0400

    fix: removed lock files and updates hook script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,11 +6,8 @@ if '{{ cookiecutter.license }}' != 'Unlicensed':
     open('LICENSE.md', 'w').write(open('licenses/{{ cookiecutter.license }}','r').read())
 shutil.rmtree('licenses')
 
-remove_lock_file = 'yarn.lock' if '{{ cookiecutter.build_system }}' == 'npm' \
-    else 'package-lock.json'
 readme_file = 'README_yarn.md' if '{{ cookiecutter.build_system }}' == 'yarn' \
     else 'README_npm.md'
 
-os.remove(remove_lock_file)
 os.rename(readme_file, 'README.md')
 for file in glob.glob('README_*.md'): os.remove(file)
